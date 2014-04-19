1e4a9eb78036f84a4fc81d63864c152e501fbf8b
Anita <anita.hammerer@gmail.com>
2014-04-19 16:05:35 +0200

    mptt-correction

--- wsgi/openshift/booklib/admin.py
+++ wsgi/openshift/booklib/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from .models import Author, Book, Category
 from suit.admin import SortableModelAdmin
+from mptt.admin import MPTTModelAdmin
+
 #from django.db import models
 
 # Register your models here.
 
-class CategoryAdmin(MPTTModelAdmin), SortableModelAdmin:
+class CategoryAdmin(MPTTModelAdmin, SortableModelAdmin):
 	mptt_level_indent = 20
 	search_fields = ('name', 'slug') 
 	prepopulated_fields = {'slug': ('name',)}
 	list_display = ('name', 'slug', 'is_active')
-	list_editable = ('is_active')
+	list_editable = ('is_active',)
 	list_display_links = ('name',)
 	sortable = 'order'
 
