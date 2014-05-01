c87e0155d0e187f77b9b5eceda52ead01b19d9c7
ah-code <anita.hammerer@gmail.com>
2014-05-01 17:11:10 +0200

    correction of urls.py

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -23,17 +23,13 @@ urlpatterns += patterns('',
         'document_root': settings.MEDIA_ROOT,
     }),
 )
-urlpatterns = patterns('',
-    ...
-    url(r'', include('social_auth.urls')),
-    ...
-)
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns += patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
@@ -43,7 +39,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('marcador.urls')),
+#    url(r'^', include('marcador.urls')),
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
