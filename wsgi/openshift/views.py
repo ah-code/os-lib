1043d11525805611beaca1de1c7bd166b92d02cb
ah-code <anita.hammerer@gmail.com>
2014-05-04 10:56:47 +0200

    openshift-correction

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -4,7 +4,4 @@ from django.contrib.auth.views import login
 from django.contrib.auth.decorators import login_required
 
 
-def home(request):
-    if (request.user.is_authenticated()):
-        return render(request, 'home/home.html')
-    return login(request, template_name='home/home.html')
+
