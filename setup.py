3e2fdcd208447db78801f1e1d9ccacc38898cd28
ah-code <anita.hammerer@gmail.com>
2014-05-20 22:11:32 +0200

    base.html extended for highlighting

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 # Put here required packages
 packages = ['Django==1.6.2', 'django-suit', 'django-mptt', 'pillow', 'django-bootstrap3', 'easy-thumbnails', 'django-social-auth', 'django-registration',
-'Whoosh', 'django-haystack', 'pip']
+'Whoosh', 'django-haystack',]
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
      packages.append('django-redis-cache')
